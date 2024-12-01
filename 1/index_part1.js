9ab143cc54fa6e5971a58673def5a5f7cca5ce99
Zerio <neoboij@gmail.com>
2024-12-01 19:47:42 +0100

    feat: add day 1 code

---
+++ 1/index_part1.js
@@ -0,0 +1,43 @@
+import path from "node:path"
+import { readFileSync } from "node:fs"
+
+const cwd = process.cwd();
+const input = readFileSync(path.join(cwd, "data.txt"))
+const lines = input.toString().split(/\r?\n/)
+
+let leftList = []
+let rightList = []
+let retVal = 0
+
+for (let i = 0; i < lines.length; i++) {
+  const splits = lines[i].split("   ")
+  if (splits.length === 2) {
+    leftList.push(parseInt(splits[0]))
+    rightList.push(parseInt(splits[1]))
+  }
+}
+
+const numSort = (a, b) => {
+  if (a < b) {
+    return -1
+  } else if (a > b) {
+    return 1
+  }
+
+  return 0
+}
+
+leftList = leftList.sort(numSort)
+rightList = rightList.sort(numSort)
+
+for (let i = 0; i < leftList.length; i++) {
+  let diff = leftList[i] - rightList[i]
+
+  if (diff < 0) {
+    diff = diff * -1
+  }
+
+  retVal = retVal + diff
+}
+
+console.log("Result: " + retVal.toString())
---
+++ 1/index_part2.js
@@ -0,0 +1,32 @@
+import path from "node:path"
+import { readFileSync } from "node:fs"
+
+const cwd = process.cwd();
+const input = readFileSync(path.join(cwd, "data.txt"))
+const lines = input.toString().split(/\r?\n/)
+
+let leftList = []
+let rightList = []
+let retVal = 0
+const cache = {}
+
+for (let i = 0; i < lines.length; i++) {
+  const splits = lines[i].split("   ")
+  if (splits.length === 2) {
+    leftList.push(parseInt(splits[0]))
+    rightList.push(parseInt(splits[1]))
+  }
+}
+
+for (let i = 0; i < leftList.length; i++) {
+  let amount = cache[leftList[i]]
+
+  if (amount === undefined) {
+    amount = rightList.filter((v) => v === leftList[i]).length
+    cache[leftList[i]] = amount
+  }
+
+  retVal = retVal + leftList[i] * amount
+}
+
+console.log("Result: " + retVal.toString())
