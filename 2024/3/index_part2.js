99f03b5fd2224ecbe65dd5138621146acda7aae4
Z3rio <neoboij@gmail.com>
2024-12-03 14:18:34 +0100

    feat: add day 3 part 2

--- 2024/3/index_part2.js
+++ 2024/3/index_part2.js
@@ -2,68 +2,27 @@ import path from "node:path"
 import { readFileSync } from "node:fs"
 
 const cwd = process.cwd();
-let data = readFileSync(path.join(cwd, "data.txt")).toString().trim()
-let foundIdx = null
-let retVal = 0
+const data = readFileSync(path.join(cwd, "data.txt")).toString().trim()
+const matches = data.match(/(mul\(\d+,\d+\))|(do(n\'t)?\(\))/g)
 let allowMultiplication = true
-const keywords = {
-  "don\'t": (raw) => {
-    if (raw === "don't()") {
-      allowMultiplication = false
-    }
-  },
-  "do": (raw) => {
-    if (raw === "do()") {
-      allowMultiplication = true
-    }
-  },
-  "mul": (raw) => {
-    if (allowMultiplication === true && raw.startsWith("mul(") && raw.endsWith(")")) {
-      const args = raw
-        .split("(")[1]
-        .split(")")[0]
-        .split(",")
-        .map(Number)
+let retVal = 0
 
-      if (args.length === 2 && args.findIndex((v) => Number.isNaN(v)) === -1) {
-        retVal = retVal + args.reduce((prev, curr) => prev * curr, 1)
-      }
+for (const match of matches) {
+  if (match === "do()") {
+    allowMultiplication = true
+  } else if (match === "don\'t()") {
+    allowMultiplication = false
+  } else if (match.startsWith("mul") && allowMultiplication === true) {
+    const args = match
+      .split("(")[1]
+      .split(")")[0]
+      .split(",")
+      .map(Number)
+
+    if (args.length === 2 && args.findIndex((v) => Number.isNaN(v)) === -1) {
+      retVal = retVal + args.reduce((prev, curr) => prev * curr, 1)
     }
   }
-}
-
-while (foundIdx === null || foundIdx !== -1) {
-  let cachedIdx = -1
-  let foundKeyword = null
-
-  for (const keyword in keywords) {
-    const idx = data.indexOf(keyword)
-
-    if (idx !== -1 && (cachedIdx === -1 || idx < cachedIdx)) {
-      cachedIdx = idx
-      foundKeyword = keyword
-    }
-  }
-
-  if (foundKeyword !== null) {
-    const splice = data.slice(cachedIdx, cachedIdx + foundKeyword.length + 1)
-    let full = splice
-
-    if (splice === foundKeyword + "(") {
-      const end = data.slice(cachedIdx + foundKeyword.length + 1).split(")")[0]
-      full = full + end + ")"
-
-      keywords[foundKeyword](full)
-
-      foundIdx = cachedIdx
-    }
-
-    data = data.slice(cachedIdx + full.length)
-
-    continue;
-  }
-
-  break;
 }
 
 console.log("Result: " + retVal.toString())
